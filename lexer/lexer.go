c0dcc1fc3c565dd67c6d17253444f65dc7cbce30
Jihyun3478 <leejihyun3478@gmail.com>
2025-11-10 13:38:26 +0900

    feat: 렉서 토큰화 기능 확장
    렉서 토큰화 기능 확장
    - 식별자(identifier) 읽기 기능 구현
    - 숫자 리터럴 읽기 기능 구현
    - 공백 문자 스킵 기능 구현
    - 각 함수에 역할 설명 주석 추가

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -4,7 +4,7 @@ import (
 	"github.com/Jihyun3478/logi-lang/token"
 )
 
-
+// 소스 코드를 토큰으로 변환하는 렉서
 type Lexer struct {
 	input string
 	position int     // 입력에서 현재 위치(현재 문자를 가리킴)
@@ -12,24 +12,18 @@ type Lexer struct {
 	ch byte          // 현재 조사하고 있는 문자
 }
 
+// 주어진 입력으로 새 렉서를 생성하고 첫 문자를 읽음
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	l.readChar()
 	return l
 }
 
-func (l *Lexer) readChar() {
-	if l.readPosition >= len(l.input) {
-		l.ch = 0
-	} else {
-		l.ch = l.input[l.readPosition]
-	}
-	l.position = l.readPosition
-	l.readPosition += 1
-}
-
+// 현재 문자를 검사하여 해당하는 토큰을 반환하고 다음 문자로 이동
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhitespace()
 
 	switch l.ch {
 	case '=':
@@ -51,12 +45,70 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
-
 	l.readChar()
 	return tok
 }
 
+// 문자가 알파벳이거나 언더바(_)인지 확인
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+// 주어진 타입과 문자로 새 토큰을 생성
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+// 연속된 문자들을 읽어 식별자(변수명, 키워드 등)를 반환
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// 다음 문자를 읽고 입력에서 위치를 전진시킨 후 EOF이면 0을 설정
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+// 백, 탭, 개행 등 whitespace 문자들을 건너뜀
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+// 연속된 숫자들을 읽어 숫자 리터럴 문자열을 반환
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// 문자가 숫자(0-9)인지 확인
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
--- token/token.go
+++ token/token.go
@@ -28,3 +28,16 @@ const (
 	FUNCTION = "FUNCTION"
 	LET = "LET"
 )
+
+var keywords = map[string]TokenType {
+	"fn": FUNCTION,
+	"let": LET,
+}
+
+// 주어진 식별자가 예약어(키워드)인지 확인하고, 예약어면 해당 토큰 타입을, 아니면 IDENT를 반환
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
