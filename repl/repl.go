0d977e26276ad7edce7952541482d0c778aa416b
Jihyun3478 <leejihyun3478@gmail.com>
2025-11-13 21:01:59 +0900

    feat: REPL에 Logi 로고 추가
    REPL에 Logi 로고 추가
    
    - 로고 상수 정의
    - 파서 에러 출력 시 로고 표시

--- repl/repl.go
+++ repl/repl.go
@@ -6,10 +6,18 @@ import (
 	"io"
 
 	"github.com/Jihyun3478/logi-lang/lexer"
-	"github.com/Jihyun3478/logi-lang/token"
+	"github.com/Jihyun3478/logi-lang/parser"
 )
 
 const PROMPT = ">> "
+const LOGI_LOGO = `
+ ██╗      ██████╗  ██████╗ ██╗
+ ██║     ██╔═══██╗██╔════╝ ██║
+ ██║     ██║   ██║██║  ███╗██║
+ ██║     ██║   ██║██║   ██║██║
+ ███████╗╚██████╔╝╚██████╔╝██║
+ ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝
+`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -23,9 +31,24 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, LOGI_LOGO)
+	io.WriteString(out, "Woops! We Ran into some business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t" + msg + "\n")
 	}
 }
