996eaaeaaa1d791fe179d3e11c606d0467d546df
Jihyun3478 <leejihyun3478@gmail.com>
2025-11-14 01:21:29 +0900

    feat: 불리언 평가 구현 및 테스트 추가
    불리언 평가 구현 및 테스트 추가
    
    - Boolean 리터럴 평가
    - TRUE/FALSE/NULL 싱글톤
    - 평가기 테스트 프레임워크 구축

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"github.com/Jihyun3478/logi-lang/object"
 )
 
+var (
+	NULL = &object.Null{}
+	TRUE = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 // AST 노드를 평가하여 객체로 변환
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
@@ -19,7 +25,11 @@ func Eval(node ast.Node) object.Object {
 	// 표현식
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
+
 	return nil
 }
 
@@ -32,4 +42,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
